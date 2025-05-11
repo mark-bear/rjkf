57557d5dbab54ef3e1cc33073f325db752c8468a
John von Neumann <m15271610278@163.com>
2025-05-11 16:14:33 +0800

    freeze

--- source/test1.py
+++ source/test1.py
@@ -10,33 +10,36 @@ def get_prompt_from_txt(file_path):
     prompts=[_[2:-1] for _ in prompts.strip().split("TASK")[1:]]
     return prompts
 
-def generare_code(prompt):
+def generare_code(prompt,model_path):
+    tokenizer = AutoTokenizer.from_pretrained(model_path,trust_remote_code=True)
+    model = AutoModelForCausalLM.from_pretrained(model_path,device_map="auto")
     inputs = tokenizer(prompt, return_tensors="pt").to(device)
 
     # 生成代码（调整参数优化输出）
-    generated_ids = model.generate(
-        **inputs,
-        max_length=4196,          # 调整生成文本的最大长度
-        temperature=0.7,          # 控制随机性（0-1，值越小越确定）
-        top_p=0.9,                # 核采样参数
-        do_sample=True,           # 启用随机采样
-        pad_token_id=tokenizer.eos_token_id
-    )
+    with torch.no_grad():
+        generated_ids = model.generate(
+            **inputs,
+            max_length=8192,          # 调整生成文本的最大长度
+            temperature=0.7,          # 控制随机性（0-1，值越小越确定）
+            top_p=0.9,                # 核采样参数
+            do_sample=True,           # 启用随机采样
+            pad_token_id=tokenizer.eos_token_id
+        )
+    del model,inputs
+    torch.cuda.empty_cache()  # 清空缓存
 
     # 解码生成的代码
     generated_code = tokenizer.decode(generated_ids[0], skip_special_tokens=True)
     return generated_code
 
 if __name__ == "__main__":
-    tokenizer = AutoTokenizer.from_pretrained("../../codegen25",trust_remote_code=True)
-    model = AutoModelForCausalLM.from_pretrained("../../codegen25",device_map="auto")
-    prompts= get_prompt_from_txt("prompt_of_CodeGen.txt")
+    prompts= get_prompt_from_txt("prompt_of_CodeGen_EN.txt")
     output_dir="../output"
     for prompt in prompts:
         print(f"doing{prompts.index(prompt)+1}:{prompt}")
         with open(f"{output_dir}/output_{prompts.index(prompt)}.txt", "w") as f:
             start_time=time.time()
-            output=generare_code(prompt)
+            output=generare_code(prompt,model_path="../../codegen25")
             end_time=time.time()
             f.write(f"generate time:{end_time-start_time}\n")
             f.write(output)
